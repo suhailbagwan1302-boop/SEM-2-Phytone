02fac7f60c53ec880d190927f17df72dc665ded1
suhailbagwan1302-boop <suhailbagwan1302@gmail.com>
2026-03-10 23:24:48 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,24 @@
+#tuple operation python
+#1. create and acess tuple
+t1=(10,20,30,40)
+print("tuple:",t1)
+print("frist element:",t1[0])
+print("last element:",t1[3])
+
+
+
+#2. nested tuple
+print("nested tuple")
+nested_tuple=(1,2,(3,4,5),6)
+print("nested tuple:",nested_tuple)
+print("access nested element:",
+      nested_tuple[2][1])
+
+#3. Repetition of Tuple
+
+print(" Repetition of Tuple")
+t2 = (7, 8)
+repeated_tuple = t2 * 3
+print("Original Tuple:", t2)
+print("Repeated Tuple:", repeated_tuple)
+
